cdff71e361b089a40c68d6fa6a764744bebae468
ansonla3 <lautszhinganson@gmail.com>
2021-01-20 15:14:33 +0800

    add CSV & writer

--- index.js
+++ index.js
@@ -1,36 +1,38 @@
-const axios = require('axios');
 const saleTxs = require('./dot_sale_tx.json');
 const { encodeAddress } = require('@polkadot/util-crypto');
 
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-// TODO. Write to csv file
-// const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-// const csvWriter = createCsvWriter({
-//     path: 'file.csv',
-//     header: [
-//         {id: 'name', title: 'NAME'},
-//         {id: 'lang', title: 'LANGUAGE'},
-//         {id: 'age', title: 'AGE'}
+const header = [
+  {id: 'address', title: 'ADDRESS'},
+  {id: 'amount', title: 'AMOUNT'},
+  {id: 'hash', title: 'HASH'}
+];
 
-//     ]
-// });
+const csvWriterNotInSale = createCsvWriter({
+    path: 'notInSaleSite.csv',
+    header: header
+});
 
-// const records = [
-//   {name: 'Bob',  lang: 'French, English', age: 'Anson'},
-//   {name: 'Mary', lang: 'English', age: 'Peter'}
-// ];
+const csvWriteMultipleContribution = createCsvWriter({
+  path: 'multipleContribution.csv',
+  header: header
+});
 
-// csvWriter.writeRecords(records)       // returns a promise
-//   .then(() => {
-//       console.log('...Done');
-//   });
-
+const csvWriterNormalContribution = createCsvWriter({
+  path: 'normalContribution.csv',
+  header: header
+});
 
 (async () => {
   try {
-    let counter = 0;              // Count the no. of txs sussessfully contribution through the sale website
     let notInSaleSiteCounter = 0; // Count the no. of txs that is not through the sale website
     const txHash = [];
+    const contributorAddrs = [];
+    const notInSaleContributions = [];
+    const multipleContributions = [];
+    const normalContributions = [];
+    let checkCounter = 0;
 
     const txs = saleTxs.txs;
     txs.forEach( tx => {
@@ -44,19 +46,50 @@ const { encodeAddress } = require('@polkadot/util-crypto');
           console.log("Contribution BTC Address:", tx.inputs[0].prev_out.addr);
           console.log("Contribution Amount:", tx.inputs[0].prev_out.value);
           console.log(`Polkadot Address: ${encodeAddress(`0x${out.script.substring(4)}`,0)}`);
-          counter += 1;
+
+          if (!contributorAddrs.includes(tx.inputs[0].prev_out.addr)) {
+            contributorAddrs.push(tx.inputs[0].prev_out.addr);
+            normalContributions.push({address: tx.inputs[0].prev_out.addr, amount: tx.inputs[0].prev_out.value, hash: tx.hash});
+          } else {
+            console.log("Multiple contribution detected address :", tx.inputs[0].prev_out.addr, tx.hash);
+            multipleContributions.push({address: tx.inputs[0].prev_out.addr, amount: tx.inputs[0].prev_out.value, hash: tx.hash});
+          }
         }
       })
 
       if (txOutList.length === outCounter) {
         notInSaleSiteCounter += 1;
         txHash.push(tx.hash);
+        notInSaleContributions.push({address: tx.inputs[0].prev_out.addr, amount: tx.inputs[0].prev_out.value, hash: tx.hash});
+
       }
+
+      checkCounter += 1;
     });
 
-    console.log("notInSaleSiteCounter: ", notInSaleSiteCounter);
+    csvWriterNotInSale.writeRecords(notInSaleContributions)
+    .then(() => {
+        console.log('NotInSale.csv ...Done');
+    });
+
+    csvWriteMultipleContribution.writeRecords(multipleContributions)
+    .then(() => {
+      console.log('multipleContribution.csv ...Done');
+    });
+
+    csvWriterNormalContribution.writeRecords(normalContributions)
+    .then(() => {
+      console.log('normalContribution.csv ...Done');
+    });
+
+
+    console.log("No. of notInSaleSiteCounter: ", notInSaleSiteCounter);
     console.log("Those notInSaleSite Hashes: ", txHash);
-    console.log("Counter: ", counter);
+
+    console.log("No. of normalContributions: ", normalContributions.length);
+    console.log("No. of multipleContributions: ", multipleContributions.length);
+
+    console.log("No. of txs have been checked: ", checkCounter);
 
   } catch (error) {
     console.log(error.response.body);
